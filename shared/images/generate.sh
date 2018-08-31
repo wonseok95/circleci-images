5ba56a7990dd4c257b877b202228f6b875296893
Ryan W. O'Hara <ryan@ryanohara.co>
2018-08-31 12:02:42 -0600

    Move the variants declaration

--- shared/images/generate.sh
+++ shared/images/generate.sh
@@ -37,10 +37,10 @@ function find_tags_and_aliases() {
 SHARED_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 TEMPLATE=${TEMPLATE:-basic}
+VARIANTS=${VARIANTS:-none}
 if [[ ${VARIANTS[@]} = *"browsers"* ]]
   then VARIANTS=($(echo ${VARIANTS[@]} | sed -E 's/([^ ]*browsers)/\1 \1-legacy/g')) 
 fi
-VARIANTS=${VARIANTS:-none}
 
 function find_template() {
   # find the right template - start with invoker path
