0806400564a81d83b5ee3dec79efc373db8e53cc
iynere <iynere@users.noreply.github.com>
2018-12-10 19:43:02 -0500

    fix sed

--- shared/images/build.sh
+++ shared/images/build.sh
@@ -91,7 +91,7 @@ function run_goss_tests() {
     export GOSS_OPTS="--format junit"
     results=$(dgoss run $IMAGE_NAME-goss)
     echo "${results#*<?xml version=\"1.0\" encoding=\"UTF-8\"?>}" | \
-      sed 's|testsuite name="goss"|testsuite name="$IMAGE_NAME"|g' >> \
+      sed "s|testsuite name=\"goss\"|testsuite name=\"$IMAGE_NAME\"|g" >> \
       ~/circleci-bundles/test-results/$PLATFORM/results.xml
 
     echo "----------------------------------------------------------------------------------------------------"
